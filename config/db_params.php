b97238a4b3bb7531b9232551f6245aea2ee69575
Dmytro <dmytro.v.levchenko@gmail.com>
2021-07-04 12:00:44 +0300

    Add index file and same config

---
+++ config/db_params.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'host' => 'localhost',
+    'dbname' => 'weblion_db',
+    'user' => 'weblion',
+    'password' => 'weblion',
+);
---
+++ views/layouts/footer.php
@@ -0,0 +1,32 @@
+                    <!-- Footer Start -->
+                    <div class="col-md-12 page-body margin-top-50 footer">
+                        <footer>
+                            <ul class="menu-link">
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="about.html">About</a></li>
+                                <li><a href="work.html">Work</a></li>
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+
+                            <p>© Copyright 2016 DevBlog. All rights reserved</p>
+                        </footer>
+                    </div>
+                    <!-- Footer End -->
+                </div>
+                <!-- Blog Post (Right Sidebar) End -->
+                </div>
+            </div>
+        </div>
+
+        <!-- Back to Top Start -->
+        <a href="#" class="scroll-to-top"><i class="fa fa-long-arrow-up"></i></a>
+        <!-- Back to Top End -->
+
+        <!-- All Javascript Plugins  -->
+        <script type="text/javascript" src="/mysite/template/js/jquery.min.js"></script>
+        <script type="text/javascript" src="/mysite/template/js/plugin.js"></script>
+
+        <!-- Main Javascript File  -->
+        <script type="text/javascript" src="/mysite/template/js/scripts.js"></script>
+    </body>
+</html>
